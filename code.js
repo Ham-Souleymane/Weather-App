669ea8594392b637120c2aa4644c59ef97b83259
Souleyman <142159569+Ham-Souleymane@users.noreply.github.com>
2025-01-23 12:57:31 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,16 +1,11 @@
 const temp_area=document.querySelector('.temp');
-
 const loc=document.querySelector('.loc');
-
 const det=document.querySelector('.weather_det');
 const search=document.querySelector('.search');
 
 
-//<span id="city"></span>, <span id="country" ></span>
-
-
 const apikey='dddaebdb79f9b6d3f1c14f3f7ac2c013';
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
 
  async function weather(City_Name){
 const res= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${City_Name}&appid=${apikey}`)
